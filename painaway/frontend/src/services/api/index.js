bfd0a0534677e416cf862bba4b953a347a840565
Anya Zharikova <kannie327@gmail.com>
2025-06-19 20:36:53 +0200

    Add new hooks

--- painaway/frontend/src/services/api/index.js
+++ painaway/frontend/src/services/api/index.js
@@ -1,1 +1,11 @@
-export { authApi, useSignupUserMutation, useLoginUserMutation } from './authApi.js'
+export {
+  authApi,
+  useSignupUserMutation,
+  useLoginUserMutation,
+} from './authApi.js'
+export {
+  chatsApi,
+  useGetChatsQuery,
+  useAddChatMutation,
+  useDeleteChatMutation,
+} from './chatsApi.js'
