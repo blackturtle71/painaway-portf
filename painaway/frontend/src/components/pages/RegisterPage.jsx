0068350ff477d5733acf66c26f21146eb49154ef
Anya Zharikova <kannie327@gmail.com>
2025-06-23 18:39:02 +0200

    Refactor formik and props

--- painaway/frontend/src/components/pages/RegisterPage.jsx
+++ painaway/frontend/src/components/pages/RegisterPage.jsx
@@ -8,6 +8,7 @@ import { toast } from 'react-toastify'
 import RegisterCard from '../ui/RegisterCard.jsx'
 import { useSignupUserMutation } from '../../services/api/authApi.js'
 import { easySignUpSchema } from '../../validation/validationSchema.js'
+import { getDays, getMonths, getYears } from '../../helpers/dateUtils.js'
 import { uiRoutes } from '../../routes.js'
 
 const RegisterPage = () => {
@@ -16,6 +17,19 @@ const RegisterPage = () => {
   const inputRef = useRef()
   const [signupUser] = useSignupUserMutation()
   const [registerFailed, setRegisterFailed] = useState(false)
+  const messages = {
+    minLength: t('registerRules.minLength'),
+    required: t('errors.required'),
+    birthType: t('registerRules.birthType'),
+    birthDay: t('registerRules.birthDay'),
+    birthMonth: t('registerRules.birthMonth'),
+    birthYear: t('registerRules.birthYear'),
+    genderError: t('errors.genderError'),
+    loginMessage: t('registerRules.login'),
+    emailMessage: t('registerRules.email'),
+    passwordMessage: t('registerRules.password'),
+    passwordConfirmation: t('registerRules.passwordConfirmation'),
+  }
 
   useEffect(() => {
     inputRef.current.focus()
@@ -23,21 +37,32 @@ const RegisterPage = () => {
 
   const formik = useFormik({
     initialValues: {
-      username: '',
+      surname: '',
+      name: '',
+      patronymic: '',
+      birthDay: '',
+      birthMonth: '',
+      birthYear: '',
+      sex: '',
+      login: '',
       email: '',
       password: '',
       passwordConfirmation: '',
     },
-    validationSchema: easySignUpSchema(t('registrationRules.name'), t('registrationRules.password'), t('errors.required'), t('registrationRules.passwordConfirmation')),
+    validationSchema: easySignUpSchema(messages),
     validateOnBlur: true,
     validateOnChange: false,
     onSubmit: async (values) => {
       try {
         await signupUser({
-          username: values.username,
+          username: values.login,
           email: values.email,
-          password1: values.password,
-          password2: values.passwordConfirmation,
+          password: values.password,
+          first_name: values.name,
+          last_name: values.surname,
+          father_name: values.patronymic,
+          sex: values.sex,
+          date_of_birth: `${values.birthYear}-${values.birthMonth.padStart(2, '0')}-${values.birthDay.padStart(2, '0')}`,
         }).unwrap()
 
         toast.success(t('success.registration'))
@@ -46,11 +71,19 @@ const RegisterPage = () => {
       }
       catch (err) {
         console.log(err.data)
-        if (err.response.username && err.response.status === 400) {
+        const errors = err.data || {}
+
+        if (errors.username) {
           toast.error(t('errors.userExists'))
           setRegisterFailed(true)
         }
-        else if (err.status) {
+
+        if (errors.email) {
+          toast.error(t('errors.emailExists'))
+          setRegisterFailed(true)
+        }
+
+        if (err.status >= 500) {
           toast.error(t('errors.serverError'))
         }
         else {
@@ -60,28 +93,68 @@ const RegisterPage = () => {
     },
   })
 
+  const inputFields = {
+    surname: {
+      field: 'surname', type: 'text', placeholder: t('form.placeholders.surname'),
+    },
+    name: {
+      field: 'name', type: 'text', placeholder: t('form.placeholders.name'),
+    },
+    patronymic: {
+      field: 'patronymic', type: 'text', placeholder: t('form.placeholders.patronymic'),
+    },
+    login: {
+      field: 'login', type: 'text', placeholder: t('form.placeholders.login'),
+    },
+    email: {
+      field: 'email', type: 'email', placeholder: t('form.placeholders.email'),
+    },
+    password: {
+      field: 'password', type: 'password', placeholder: t('form.placeholders.password'),
+    },
+    passwordConfirmation: {
+      field: 'passwordConfirmation', type: 'password', placeholder: t('form.placeholders.passwordConfirmation'),
+    },
+  }
+
+  const dateFields = {
+    birthDay: {
+      ariaLabel: t('form.ariaLabels.day'), range: getDays(),
+    },
+    birthMonth: {
+      ariaLabel: t('form.ariaLabels.month'), range: getMonths(),
+    },
+    birthYear: {
+      ariaLabel: t('form.ariaLabels.year'), range: getYears(),
+    },
+  }
+
+  const genders = [
+    { gender: t('form.gender.male'), type: 'radio', id: 1 },
+    { gender: t('form.gender.female'), type: 'radio', id: 2 },
+  ]
+
   const values = {
     formik,
     title: t('registration'),
     buttonTitle: t('makeRegistration'),
-    placeholderUsername: t('placeholders.username'),
-    placeholderEmail: t('placeholders.email'),
-    placeholderPassword: t('placeholders.password'),
-    placeholderPasswordConfirmation: t('placeholders.passwordConfirmation'),
+    inputFields,
+    dateFields,
+    dateOfBirth: t('form.dateOfBirth'),
+    genders,
+    sex: t('form.gender.sex'),
     userExists: t('errors.userExists'),
     haveAccount: t('haveAccount'),
     login: t('entry'),
     path: uiRoutes.login(),
     registerFailed,
-    // usernameOnBlur: handleUsernameBlur,
-    // emailOnBlur: handleEmailBlur,
     inputRef,
   }
 
   return (
     <Container className="mt-5">
       <Row className="justify-content-center">
-        <Col md={8} lg={6} py={5}>
+        <Col md={12} lg={10} xl={9}>
           <RegisterCard values={values} />
         </Col>
       </Row>
