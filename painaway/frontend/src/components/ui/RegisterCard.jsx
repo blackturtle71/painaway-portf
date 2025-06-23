21f1ce0506c4c633729526431d735d64bebecf68
Anya Zharikova <kannie327@gmail.com>
2025-06-23 18:37:54 +0200

    Refactor layout and props in RegisterCard

--- painaway/frontend/src/components/ui/RegisterCard.jsx
+++ painaway/frontend/src/components/ui/RegisterCard.jsx
@@ -1,7 +1,9 @@
-import { Form, Card } from 'react-bootstrap'
+import { Form, Card, Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 import InputField from './InputField.jsx'
+import DateOfBirth from './DateOfBirth.jsx'
+import Gender from './Gender.jsx'
 import SubmitButton from './SubmitButton.jsx'
 
 const RegisterCard = (props) => {
@@ -10,47 +12,62 @@ const RegisterCard = (props) => {
     formik,
     title,
     buttonTitle,
-    placeholderUsername,
-    placeholderEmail,
-    placeholderPassword,
-    placeholderPasswordConfirmation,
+    inputFields,
+    dateFields,
+    dateOfBirth,
+    genders,
+    sex,
     userExists,
     haveAccount,
     login,
     path,
     registerFailed,
-    usernameOnBlur,
-    emailOnBlur,
     inputRef,
   } = values
 
-  const propsUsername = {
+  const propsSurname = {
     formik,
-    field: 'username',
-    type: 'text',
-    placeholder: placeholderUsername,
-    failed: (formik.errors.username && formik.touched.username) || registerFailed,
-    usernameOnBlur,
+    ...inputFields.surname,
+    failed: formik.errors.surname && formik.touched.surname,
     inputRef,
-    error: formik.errors.username,
+    error: formik.errors.surname,
+  }
+
+  const propsName = {
+    formik,
+    ...inputFields.name,
+    failed: formik.errors.name && formik.touched.name,
+    inputRef: null,
+    error: formik.errors.name,
+  }
+
+  const propsPatronymic = {
+    formik,
+    ...inputFields.patronymic,
+    failed: formik.errors.patronymic && formik.touched.patronymic,
+    inputRef: null,
+    error: formik.errors.patronymic,
+  }
+
+  const propsLogin = {
+    formik,
+    ...inputFields.login,
+    failed: (formik.errors.login && formik.touched.login) || registerFailed,
+    inputRef: null,
+    error: formik.errors.login,
   }
 
   const propsEmail = {
     formik,
-    field: 'email',
-    type: 'text',
-    placeholder: placeholderEmail,
+    ...inputFields.email,
     failed: (formik.errors.email && formik.touched.email) || registerFailed,
-    emailOnBlur,
     inputRef: null,
     error: formik.errors.email,
   }
 
   const propsPassword = {
     formik,
-    field: 'password',
-    type: 'password',
-    placeholder: placeholderPassword,
+    ...inputFields.password,
     failed: (formik.errors.password && formik.touched.password),
     inputRef: null,
     error: formik.errors.password,
@@ -58,9 +75,7 @@ const RegisterCard = (props) => {
 
   const propsPasswordConfirmationValues = {
     formik,
-    field: 'passwordConfirmation',
-    type: 'password',
-    placeholder: placeholderPasswordConfirmation,
+    ...inputFields.passwordConfirmation,
     failed: (
       (formik.errors.passwordConfirmation && formik.touched.passwordConfirmation)
       || registerFailed
@@ -69,16 +84,68 @@ const RegisterCard = (props) => {
     error: registerFailed ? userExists : formik.errors.passwordConfirmation,
   }
 
+  const propsBirth = {
+    formik,
+    label: dateOfBirth,
+    fields: [
+      { field: 'birthDay', ...dateFields.birthDay },
+      { field: 'birthMonth', ...dateFields.birthMonth },
+      { field: 'birthYear', ...dateFields.birthYear },
+    ],
+  }
+
+  const propsGender = { formik, genders, sex }
+
   return (
     <Card className="shadow-sm">
-      <Card.Body className="p-lg-5">
+      <Card.Body className="p-4">
         <h1 className="mb-4">{title}</h1>
 
+        {/* Фамилия Имя Отчество */}
         <Form onSubmit={formik.handleSubmit}>
-          <InputField values={propsUsername} />
-          <InputField values={propsEmail} />
-          <InputField values={propsPassword} />
-          <InputField values={propsPasswordConfirmationValues} />
+          <Row>
+            <Col md={4}>
+              <InputField values={propsSurname} />
+            </Col>
+            <Col md={4}>
+              <InputField values={propsName} />
+            </Col>
+            <Col md={4}>
+              <InputField values={propsPatronymic} />
+            </Col>
+          </Row>
+
+          {/* Дата рождения и Пол */}
+          <Row>
+            <Col md={8}>
+              <Col>
+                <DateOfBirth values={propsBirth} />
+              </Col>
+            </Col>
+            <Col md={4}>
+              <Gender values={propsGender} />
+            </Col>
+          </Row>
+
+          {/* Логин и Почта */}
+          <Row>
+            <Col md={6}>
+              <InputField values={propsLogin} />
+            </Col>
+            <Col md={6}>
+              <InputField values={propsEmail} />
+            </Col>
+          </Row>
+
+          {/* Пароль и Подтверждение */}
+          <Row>
+            <Col md={6}>
+              <InputField values={propsPassword} />
+            </Col>
+            <Col md={6}>
+              <InputField values={propsPasswordConfirmationValues} />
+            </Col>
+          </Row>
 
           <SubmitButton values={{ formik, buttonTitle }} />
         </Form>
