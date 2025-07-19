a414b49c8d90116e9c83122d71d79db3cccaf5b2
Anya Zharikova <kannie327@gmail.com>
2025-07-19 13:12:15 +0200

    Fix layout and add data retrieval

--- painaway/frontend/src/components/pages/PatientsPage.jsx
+++ painaway/frontend/src/components/pages/PatientsPage.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useDispatch } from 'react-redux'
 
+import { useGetLinksQuery } from '../../services/api/linksApi.js'
 import { openModal } from '../../slices/modalsSlice.js'
 import InputField from '../ui/InputField.jsx'
 import SubmitButton from '../ui/SubmitButton.jsx'
@@ -11,6 +12,8 @@ const PatientsPage = () => {
   const { t } = useTranslation()
   const dispatch = useDispatch()
   const [searchSurname, setSearchSurname] = useState()
+  const { data: linksData } = useGetLinksQuery()
+  const acceptedPatients = linksData?.filter(link => link.status === 'accepted') || []
 
   const handleSearch = () => {
     console.log('Finding...')
@@ -36,7 +39,40 @@ const PatientsPage = () => {
         />
       </div>
 
-      <div></div>
+      <div className="accepted-patients">
+        {acceptedPatients.map(patient => (
+          <div key={patient.id} className="patient-card">
+            <div className="patient-info">
+              <span className="full-name">
+                {patient.patient.last_name}
+                {' '}
+                {patient.patient.first_name}
+                {' '}
+                {patient.patient.father_name}
+              </span>
+              <span className="birthday">
+                {patient.patient.date_of_birth}
+              </span>
+            </div>
+            <div className="patient-diagnosis">
+              <div className="field-row">
+                <label htmlFor="diagnosis">
+                  {t('patientsPage.diagnosis')}
+                  :
+                </label>
+                <span className="diagnosis">Тут диагноз</span>
+              </div>
+              <div className="field-row">
+                <label htmlFor="new-entries">
+                  {t('patientsPage.newEntries')}
+                  :
+                </label>
+                <span className="new-entries">Тут новые записи</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
 
       <aside className="new-requests">
         <button
