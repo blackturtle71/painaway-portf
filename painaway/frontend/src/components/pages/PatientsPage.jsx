1d5f7c04d08011a117a305cd86eb09fee4a83193
Anya Zharikova <kannie327@gmail.com>
2025-07-27 21:15:00 +0200

    Add search by last name

--- painaway/frontend/src/components/pages/PatientsPage.jsx
+++ painaway/frontend/src/components/pages/PatientsPage.jsx
@@ -1,27 +1,44 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useDispatch } from 'react-redux'
 
 import { useGetLinksQuery } from '../../services/api/linksApi.js'
 import { openModal } from '../../slices/modalsSlice.js'
 import InputField from '../ui/InputField.jsx'
-import SubmitButton from '../ui/SubmitButton.jsx'
 import Modal from '../modal/Modal.jsx'
+import { useNavigate } from 'react-router-dom'
+import { uiRoutes } from '../../routes.js'
 
 const PatientsPage = () => {
   const { t } = useTranslation()
   const dispatch = useDispatch()
-  const [searchSurname, setSearchSurname] = useState()
-  const { data: linksData } = useGetLinksQuery()
-  const acceptedPatients = linksData?.filter(link => link.status === 'accepted') || []
+  const navigate = useNavigate()
+  const { data: linksData, isLoading, isError } = useGetLinksQuery()
 
-  const handleSearch = () => {
-    console.log('Finding...')
-  }
+  const acceptedPatients = useMemo(() => linksData?.filter(link => link.status === 'accepted') || [], [linksData])
 
-  const hadnleOpenModal = () => {
+  const [searchSurname, setSearchSurname] = useState('')
+
+  const filteredPatients = useMemo(() => {
+    const search = searchSurname.trim().toLowerCase()
+
+    if (!search) return acceptedPatients
+
+    return acceptedPatients.filter(p => p.patient.last_name.toLowerCase().includes(search))
+  }, [acceptedPatients, searchSurname])
+
+  const handleChange = e => setSearchSurname(e.target.value)
+
+  const handleOpenModal = () => {
     dispatch(openModal({ type: 'linkRequest' }))
   }
+
+  const handlePatientClick = (patientId) => {
+    navigate(uiRoutes.patientCard(patientId))
+  }
+
+  if (isLoading) return <div>Loading...</div>
+  if (isError) return <div>Error loading patient data</div>
 
   return (
     <section className="patients-section">
@@ -30,54 +47,54 @@ const PatientsPage = () => {
           name="search"
           placeholder={t('patientsPage.findPatient')}
           value={searchSurname}
-          onChange={e => setSearchSurname(e.target.value)}
+          onChange={handleChange}
           showLabel={false}
-        />
-        <SubmitButton
-          title={t('patientsPage.findButton')}
-          onClick={handleSearch}
         />
       </div>
 
       <div className="accepted-patients">
-        {acceptedPatients.map(patient => (
-          <div key={patient.id} className="patient-card">
-            <div className="patient-info">
-              <span className="full-name">
-                {patient.patient.last_name}
-                {' '}
-                {patient.patient.first_name}
-                {' '}
-                {patient.patient.father_name}
-              </span>
-              <span className="birthday">
-                {patient.patient.date_of_birth}
-              </span>
-            </div>
-            <div className="patient-diagnosis">
-              <div className="field-row">
-                <label htmlFor="diagnosis">
-                  {t('patientsPage.diagnosis')}
-                  :
-                </label>
-                <span className="diagnosis">Тут диагноз</span>
+        {filteredPatients.map((patient) => {
+          return (
+            <div
+              key={patient.id}
+              className="patient-card"
+              onClick={() => handlePatientClick(patient.patient.id)}
+            >
+              <div className="patient-info">
+                <span className="full-name">
+                  <span>{patient.patient.last_name}</span>
+                  <span>{patient.patient.first_name}</span>
+                  <span>{patient.patient.father_name}</span>
+                </span>
+                <span className="birthday">
+                  {patient.patient.date_of_birth}
+                </span>
               </div>
-              <div className="field-row">
-                <label htmlFor="new-entries">
-                  {t('patientsPage.newEntries')}
-                  :
-                </label>
-                <span className="new-entries">Тут новые записи</span>
+              <div className="patient-diagnosis">
+                <div className="field-row">
+                  <label htmlFor="diagnosis">
+                    {t('patientsPage.diagnosis')}
+                    {': '}
+                  </label>
+                  <span className="diagnosis">{patient.diagnosis?.diagnosis || 'Нет диагноза'}</span>
+                </div>
+                <div className="field-row">
+                  <label htmlFor="new-entries">
+                    {t('patientsPage.numberEntries')}
+                    {': '}
+                  </label>
+                  <span className="new-entries">Тут новые записи</span>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          )
+        })}
       </div>
 
       <aside className="new-requests">
         <button
           className="new-requests-button"
-          onClick={hadnleOpenModal}
+          onClick={handleOpenModal}
         >
           {t('patientsPage.newRequests')}
         </button>
