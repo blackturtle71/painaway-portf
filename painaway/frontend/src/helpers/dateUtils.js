017f6bb9da4d5a919aa7d3276c9efe7fabd06609
Anya Zharikova <kannie327@gmail.com>
2025-07-27 20:51:17 +0200

    Fix dateUtils

--- painaway/frontend/src/helpers/dateUtils.js
+++ painaway/frontend/src/helpers/dateUtils.js
@@ -1,11 +1,30 @@
-export const getDays = () => Array.from(
-  { length: 31 }, (_, i) => String(i + 1).padStart(2, '0'),
-)
+export const formatDate = (isoDate) => {
+  const date = new Date(isoDate)
+  return date.toLocaleDateString('ru-RU')
+}
 
-export const getMonths = () => Array.from(
-  { length: 12 }, (_, i) => String(i + 1).padStart(2, '0'),
-)
+export const formatTime = (isoDate) => {
+  const formattedTime = date.toLocaleTimeString('ru-RU', {
+    hour: '2-digit',
+    minute: '2-digit',
+  })
+  return formattedTime
+}
 
-export const getYears = (from = 1920, to = new Date().getFullYear()) => Array.from(
-  { length: to - from + 1 }, (_, i) => String(to - i),
-)
+export const formatDateTime = (isoDate) => {
+  const date = new Date(isoDate)
+  
+  const formattedDate = date.toLocaleDateString('ru-RU', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  })
+
+  const formattedTime = date.toLocaleTimeString('ru-RU', {
+    hour: '2-digit',
+    minute: '2-digit',
+  })
+
+  return `${formattedDate} (${formattedTime})`
+}
+
